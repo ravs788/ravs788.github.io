f6eb5338f228b2c654b1c75bcb4c598537b434d0
ravs788 <ravs788@gmail.com>
2021-01-13 23:23:06 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+
+    if(mySrc=='images/mybaseImage.jpg'){
+        myImage.setAttribute('src','images/image2.jpg');
+    } else {
+        myImage.setAttribute('src','images/mybaseImage.jpg');
+    }
+
+
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please enter user name');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'My Website is cool, ain\'t it '+myName+" ?";
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'My Website is cool, ain\'t it '+storedName+" ?";
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
