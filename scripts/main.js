a0c557ac269e5bb9c9af881224eca8c413b42b24
Ravi Shankar <ravs788@gmail.com>
2021-01-13 23:35:31 +0530

    Fixed Null Name Issues

--- scripts/main.js
+++ scripts/main.js
@@ -16,9 +16,13 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName(){
-    let myName = prompt('Please enter user name');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'My Website is cool, ain\'t it '+myName+" ?";
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'My Website is cool, ain\'t it '+${myName}+" ?";
+    }
 }
 
 if(!localStorage.getItem('name')){
